9d54e1c81237af274282fe94186b5aeac4eafbb6
Juan Mesa <juand.mesa@hotmail.com>
2018-03-25 18:13:07 -0500

    Primera version con React

---
+++ components/button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Button extends Component{
+  render(){
+    return(
+      <div className="Button">
+
+      </div>
+    )
+  }
+}
+
+export default Button;
